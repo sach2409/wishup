dcb3bd9e629e5b9026b4478e50b0074c717d03f9
Sachin Meena <sach2409@gmail.com>
2016-04-11 14:45:11 +0530

    store done, no luch retrieving yet

--- app/retrieve_tasks.js
+++ app/retrieve_tasks.js
@@ -2,30 +2,32 @@ var mongoose = require('mongoose');
 var Todo = require('../app/models/tasks');
 
 module.exports.index = function ( req, res, next ){
-  var user_id = req.cookies ?
-    req.cookies.user_id : undefined;
+  // var user_id = req.cookies ?
+  //   req.cookies.user_id : undefined;
 
   Todo.
-    find({ user_id : user_id }).
+    find({ user_id : req.user }).
     sort( '-due_time' ).
     exec( function ( err, todos ){
       if( err ) return next( err );
 
-      res.render( '/addtask', {
-          user : req.user
+      res.render( '/profile', {
+          user : req.user,
+          todos: todos
       });
     });
 };
 
 module.exports.create = function ( req, res, next ){
   new Todo({
-      user_id    : req.cookies.user_id,
+      user_id    : req.user,//req.cookies.user_id,
       task_detail: req.body.task_detail,
       location   : req.body.location,
       due_time	 : req.body.due_time
   }).save( function ( err, todo, count ){
     if( err ) return next( err );
 
+    console.log("stored data");
     res.redirect('/addtask');
   });
 };
--- app/routes.js
+++ app/routes.js
@@ -1,5 +1,5 @@
 var geohash = require("geohash").GeoHash;
-var tasks = require('./retrieve_tasks.js')
+var Todo = require('../app/models/tasks');
 module.exports = function(app,passport,mongoose){
 	app.get('/',function(req,res){
 		res.render('index.ejs');
@@ -30,7 +30,35 @@ module.exports = function(app,passport,mongoose){
             user : req.user
         });//, { message: req.flash('signupMessage') });
     });
-    app.post('/addtask',isLoggedIn, tasks.index);
+    app.post('/addtask',isLoggedIn, function ( req, res, next ){
+      new Todo({
+          user_id    : req.user,//req.cookies.user_id,
+          task_detail: req.body.task_detail,
+          location   : req.body.location,
+          due_time   : req.body.due_time
+      }).save( function ( err, todo, count ){
+        if( err ) return next( err );
+
+        console.log("stored data");
+        res.redirect('/addtask');
+      });
+    });
+    app.post('/profile',isLoggedIn, function ( req, res, next ){
+      // var user_id = req.cookies ?
+      //   req.cookies.user_id : undefined;
+
+      Todo.
+        find({ user_id : req.user }).
+        sort( '-due_time' ).
+        exec( function ( err, todo ){
+          if( err ) return next( err );
+
+          res.render( '/profile', {
+              user : req.user,
+              todos: todo
+          });
+        });
+    });
     app.get('/logout', function(req, res) {
         req.logout();
         res.redirect('/'); //redirect to home
