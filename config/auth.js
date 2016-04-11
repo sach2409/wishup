3b72ded0cc7331c443cbaad636b6233b7a9846cc
Sachin Meena <sach2409@gmail.com>
2016-04-11 17:29:26 +0530

    problem redirecting from fb and g+ auths

--- config/auth.js
+++ config/auth.js
@@ -1,14 +1,14 @@
 module.exports = {
 
     'facebookAuth' : {
-        'clientID'      : '924343477664345', 
-        'clientSecret'  : 'a2d01f32169d41aa75cb688c10f6e0bb', 
+        'clientID'      : 'ur api key', 
+        'clientSecret'  : 'r secret', 
         'callbackURL'   : 'http://127.0.0.1:8080/auth/facebook/callback'
     },
 
     'googleAuth' : {
-        'clientID'      : '938443435086-f9fknj063ft0gsr6hq4qd1ai1v5rg1pd.apps.googleusercontent.com',
-        'clientSecret'  : 'y41_HsP_9z_mwyhZLFIoDqbH',
+        'clientID'      : 'ue api ket',
+        'clientSecret'  : 'ur secret',
         'callbackURL'   : 'http://127.0.0.1:8080/auth/google/callback'
     }
 
